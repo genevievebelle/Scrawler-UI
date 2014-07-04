2f5dbb4b9e3973fc0135f7383c9f2641e62c5e11
HSalmon & Genevieve Parkes <pair+HSalmon.genevievebelle@devbootcamp.com>
2014-07-04 14:58:27 +1200

    chat view appends li elements

--- public/javascripts/ChatView.js
+++ public/javascripts/ChatView.js
@@ -1,6 +1,6 @@
 var ChatView = (function() {
   var appendMessageDiv = function(text) {
-    $('<div/>').text(text).appendTo(ChatWindow.chatLog);
+    $('<li/>').text(text).appendTo(ChatWindow.chatLog);
     ChatWindow.chatLog[0].scrollTop = ChatWindow.chatLog[0].scrollHeight;
   };
 
