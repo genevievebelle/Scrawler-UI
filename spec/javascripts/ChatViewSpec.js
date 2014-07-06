0c7a0a2772bb16177a3dc36281cbe545cd3ffa3a
Mikedgs <michael.douglas21@gmail.com>
2014-07-06 14:48:54 +1200

    tidied up html and css

--- spec/javascripts/ChatViewSpec.js
+++ spec/javascripts/ChatViewSpec.js
@@ -1,10 +1,3 @@
-describe("ChatView", function() {
-  describe("appendMessageDiv", function() {
-    var fixture = $("<ul class='messagesDiv'></ul>");
-    fixture.myTestedPlugin();
-    ChatView.appendMessageDiv("text");
-    expect(fixture).toContainElement('li');
-  });
-});
+
 
 
