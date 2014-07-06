d97dd9a3db3e8af9c950d5ea75f76efa73fd6613
HSalmon & Genevieve Parkes <pair+HSalmon.genevievebelle@devbootcamp.com>
2014-07-06 15:14:13 +1200

    temporarily build just 1 immortal

--- public/javascripts/ImmortalMessage.js
+++ public/javascripts/ImmortalMessage.js
@@ -1,7 +1,8 @@
 var ImmortalMessage = (function(immortalData){
 
   var buildImmortalMessage = function(immortalData) {
-    for(i = 0; i < immortalData.length; i++){
+    for(i = 0; i < 1; i++){
+    // for(i = 0; i < immortalData.length; i++){
       var content = immortalData[i].Content;
       var date = moment(immortalData[i].Time).format("MMMM Do YYYY");
       ChatView.appendImmortalList(content, immortalData[i].Id, date);
