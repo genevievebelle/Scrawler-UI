021ee6ee9a10629366128b2e8267c246c332e7cd
Mikedgs <michael.douglas21@gmail.com>
2014-07-06 15:00:35 +1200

    Tidied up code

--- public/javascripts/ImmortalMessage.js
+++ public/javascripts/ImmortalMessage.js
@@ -1,9 +1,8 @@
 var ImmortalMessage = (function(immortalData){
-
   var buildImmortalMessage = function(immortalData) {
     for(i = 0; i < immortalData.length; i++){
       var content = immortalData[i].Content;
-      var date = moment(immortalData[i].Time).format("MMMM Do YYYY");
+      var date = moment(immortalData[i].Time).format("MMMM Do");
       ChatView.appendImmortalList(content, immortalData[i].Id, date);
     };
   };
