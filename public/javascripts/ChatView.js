cf90d581daf213cb43e8765d5d1e0f269419ca4f
HSalmon & Genevieve Parkes <pair+HSalmon.genevievebelle@devbootcamp.com>
2014-07-07 13:23:52 +1200

    removed ImmortalView functionality from ChatView

--- public/javascripts/ChatView.js
+++ public/javascripts/ChatView.js
@@ -9,24 +9,6 @@ var ChatView = (function() {
     window.scrollTo(0,document.body.scrollHeight);
   };
 
-  var appendImmortalList = function(messageContent, messageId, Timestamp) {
-    $('<li/>').html(messageContent+'</br><span class="time">'+ Timestamp + '</span><span class="location">near Cuba Street</span>').appendTo(Window.immortalMessageList);
-    Window.immortalMessageList.children().last()[0].classList.add(messageId);
-  };
-
-  var rotateImmortal = function() {
-    var length = $('.immortalMessageList li').length;
-
-    $('.immortalMessageList li').eq(current).fadeOut(500, function(){
-      current++;
-      if(current === length){
-        current =  0;
-      }
-      $('.immortalMessageList li').eq(current).fadeIn(500);
-    });
-    setTimeout(rotateImmortal, 3000);
-  };
-
   var appendRoomName = function(name) {
     $(".room-name").text(name);
   }
