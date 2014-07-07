3e886e70a8314a6fd13fb3b4c5fcc43b6cbbdfc1
regan-young <im.regan.young@gmail.com>
2014-07-07 14:01:16 +1200

    Tweaked some numbers

--- public/javascripts/TrollGuard.js
+++ public/javascripts/TrollGuard.js
@@ -14,16 +14,16 @@ var Trollguard = (function(){
 
 	var allowSend = function(){
 		var currentIndex = parseInt(counter)-1;
-		var oldIndex = currentIndex-11;
+		var oldIndex = currentIndex-7;
 		var timeElapsed = lastFiveMessages[currentIndex] - lastFiveMessages[oldIndex];
 		console.log("timeElapsed: ", timeElapsed);
-		if(timeElapsed < 5000){
+		if(timeElapsed < 10000){
 			return false;
 		} else return true;
 	};
 
 	var fadeSend = function(){
-		ChatWindow.sendButton.css("background-color", "#8A8A8A");
+		Window.sendButton.css("background-color", "#8A8A8A");
 		setTimeout(restoreSend, 5000);
 	};
 
