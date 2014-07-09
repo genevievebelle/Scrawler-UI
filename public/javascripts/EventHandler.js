d5718d6240342f289adabe75d67be89918fa545f
regan-young <im.regan.young@gmail.com>
2014-07-09 16:24:57 +1200

    Slightly refactored FBmodule. Moved event handler to appropriate file

--- public/javascripts/EventHandler.js
+++ public/javascripts/EventHandler.js
@@ -12,7 +12,22 @@ var EventHandler = (function() {
     jQueryObject.addClass("red");
   };
 
+  var sendMessageClickEvent = function(event) {
+    event.preventDefault();
+    var checkIfNotSpamming = Trollguard.checkSpammer();
+    if(checkIfNotSpamming == true){
+      //if the user is not spamming, allow them to send a message.
+      FirebaseModule.sendMessagetoFireBase(Window.messageInput.val());
+      Window.messageInput.val('');
+    } else {
+      //if the user is spamming, do not send the message, fade the send button and alert them.
+      Window.fadeSendButton();
+      Window.appendSystemMessage("Messaging disabled for 5 seconds");
+    };
+  };
+
   return {
     bindClickEvents: bindClickEvents,
+    sendMessageClickEvent: sendMessageClickEvent
   };
 })();
